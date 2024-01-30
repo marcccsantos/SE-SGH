0e36e2931aa17918dd02528540fb29e890329b57
marcccsantos <148510745+marcccsantos@users.noreply.github.com>
2024-01-30 18:45:12 +0800

    search

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -3,10 +3,12 @@ import { Link } from 'react-router-dom';
 import './login.css'; 
 
 const Login = () => {
-  const [password, setPassword] = useState(false);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordVisible, setPasswordVisibility] = useState(false);
  
   const passwordVisibility = () => {
-    setPassword(!password);
+    setPasswordVisibility(!passwordVisible);
   };
 
   useEffect(() => {
@@ -16,6 +18,10 @@ const Login = () => {
       document.body.classList.remove('loginPage');
     };
   }, []);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  }
 
   return ( 
     <>
@@ -35,22 +41,24 @@ const Login = () => {
           </div>
           <img className="welcome-img" alt="" src="/welcome.png" />
 
-          <form action="">
+          <form onSubmit={handleSubmit}>
             <input
               type="text"
               placeholder="Enter Username"
               className="username"
               required
+              value = {username} onChange={(e) => {setUsername(e.target.value)}}
             />
 
             <input
-              type={password ? 'text' : 'password'}
+              type={passwordVisible ? 'text' : 'password'}
               placeholder="Enter Password"
               className="password"
               id="input"
               required
+              value = {password} onChange={(e) => {setPassword(e.target.value)}}
             />
-            {password ? (
+            {passwordVisible ? (
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="eye-btn" onClick={passwordVisibility}>
                 <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
                 <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"/>
--- src/pages/search.jsx
+++ src/pages/search.jsx
@@ -1,12 +1,27 @@
+import React, { useEffect } from 'react';
 import './search.css'; 
 import Header from '../components/header';
 import Footer from '../components/footer';
 
 const Search = () => {
+
     return (
         <>  
             <Header />
             
+            <div className="search">
+                <img className="logo-search" src="/logo-1.png" alt="" />
+                <img className="sgh-search" src="/sgh.png" alt="" />
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="search-icon">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+                </svg>
+                <form>
+                    <input className="search-input" type="text" placeholder="Search" />
+                    <button className="btn-search">Search</button>
+                </form>
+
+            </div>
+
             <Footer />
         </>
     )
