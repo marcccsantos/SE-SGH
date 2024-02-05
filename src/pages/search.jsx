f76dfadd048d13b271e32825808595afaca04bd1
marcccsantos <148510745+marcccsantos@users.noreply.github.com>
2024-02-05 22:30:59 +0800

    add record responsive

--- src/pages/search.jsx
+++ src/pages/search.jsx
@@ -22,7 +22,7 @@ const Search = () => {
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="search-icon">
                         <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
                 </svg>
-                <form >
+                <form className="searchForm">
                     <input className="search-input" name="search" type="text" placeholder="Search" />
                     <button className="btn-search">Search</button>
                 </form>
