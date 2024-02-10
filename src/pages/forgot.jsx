4f63a0b8e139e18d11918b8966b60296d1848738
marcccsantos <148510745+marcccsantos@users.noreply.github.com>
2024-02-10 12:31:32 +0800

    working forgot

--- src/pages/forgot.jsx
+++ src/pages/forgot.jsx
@@ -1,14 +1,52 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
+import { sendPasswordResetEmail } from "firebase/auth";
+import { auth } from "../firebase";
+import { Link, useNavigate } from 'react-router-dom';
 import './forgot.css'; 
 
+function ForgotPassword(){
+    const history = useNavigate();
 
-const forgot_pass = () => {
+    const handleSubmit = async(e)=>{
+        e.preventDefault()
+        const emalVal = e.target.email.value;
+        sendPasswordResetEmail(auth,emalVal).then(data=>{
+            alert("Password reset email sent successfully!")
+            history("/")
+        }).catch(err=>{
+            alert(err.code)
+        })
+    }
 
-  return (
-    <>
-      <h1>Hello Forgot Password</h1>
-    </>
-  );
-};
+    useEffect(() => {
+      document.body.classList.add('forgotPage');
+  
+      return () => {
+        document.body.classList.remove('forgotPage');
+      };
+    }, []); 
 
-export default forgot_pass;
+    return(
+        <div className="forgot">
+          <div className="forgot-card">
+              <h1 className="forgot-text">Forgot Password?</h1>
+              <p className="info-forgot">Please enter your email address below to receive a password reset link.</p>
+              <form onSubmit={(e)=>handleSubmit(e)}>
+                  <input 
+                    className="email-forgot"
+                    name="email"
+                    placeholder="Email Address" 
+                    required
+                  />
+                  <button className="btn-reset">Send Reset Link</button>
+              </form>
+              <div className="back">
+                <Link to="/" className="forgot-back">
+                  Back to log in
+                </Link>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default ForgotPassword;
