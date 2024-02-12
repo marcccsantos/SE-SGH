82390b810a10fa612053468b05f754b664ffa405
marcccsantos <148510745+marcccsantos@users.noreply.github.com>
2024-02-12 10:29:34 +0800

    updated header

--- src/pages/search.jsx
+++ src/pages/search.jsx
@@ -3,6 +3,7 @@ import './search.css';
 import Header from '../components/header';
 import Footer from '../components/footer';
 
+
 const Search = () => {
     useEffect(() => {
         document.body.classList.add('searchPage');
@@ -24,7 +25,12 @@ const Search = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="search-icon">
                             <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
                         </svg>
-                        <input className="search-input" name="search" type="text" placeholder="Search Record" />
+                        <input 
+                            className="search-input" 
+                            name="search" 
+                            type="text" 
+                            placeholder="Search Record" 
+                        />
                     </div>
                     <button className="btn-search">Search</button>
                 </form>
