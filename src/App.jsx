b04830ab3c68e1cceeaca81d9f2a9665f9f4773c
marcccsantos <148510745+marcccsantos@users.noreply.github.com>
2024-02-28 23:10:52 +0800

    employee side restricted

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import {BrowserRouter as Router, Routes,Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./firebase";
-import { ProtectedRoute } from "./components/protectedRoute"
+import { ProtectedRoute } from "./components/protectedRoute";
 import Login from "./pages/login";
 import Forgot from "./pages/forgot";
 import Search from "./pages/search";
@@ -14,104 +14,137 @@ import ArchiveRecord from "./pages/ArchiveRecord";
 import Payroll from "./pages/Payroll";
 import PayrollFinal from "./pages/PayrollFinal";
 import NotFound from "./pages/not-found";
-import DTR from "./pages/DTR"
-import EmployeeProfile from "./pages/EmployeeProfile"
+import DTR from "./pages/DTR";
+import EmployeeProfile from "./pages/EmployeeProfile";
+import Unauthorized from "./pages/Unauthorized";
 import { useEffect, useState } from "react";
 
 const App = () => {
-    const [user, setUser] = useState(null);
-    const [isFetching, setIsFetching] = useState(true);
-    useEffect (() => {
-        const unsubscribe = onAuthStateChanged(auth, (user) => {
-            if(user){
-                setUser(user);
-                setIsFetching(false);
-                return;
+  const [user, setUser] = useState(null);
+  const [isFetching, setIsFetching] = useState(true);
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+        setIsFetching(false);
+        return;
+      }
+      setUser(null);
+      setIsFetching(false);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  if (isFetching) {
+    return <h2>Loading...</h2>;
+  }
+
+  return (
+    <div className="app">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/forgot" element={<Forgot />} />
+          <Route
+            path="/search"
+            element={
+              <ProtectedRoute user={user}>
+                <Search />
+              </ProtectedRoute>
             }
-            setUser(null);
-            setIsFetching(false);
-        });
-        return () => unsubscribe();
-    }, []);
+          />
+          <Route
+            path="/ViewRecord/:searchQuery"
+            element={
+              <ProtectedRoute user={user}>
+                <ViewRecord />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/ViewRecord"
+            element={
+              <ProtectedRoute user={user}>
+                <ViewRecord />
+              </ProtectedRoute>
+            }
+          />
 
-    if(isFetching){
-        return <h2>Loading...</h2>
-    }
+          <Route
+            path="/ViewProfile/:employeeID"
+            element={
+              <ProtectedRoute user={user}>
+                <ViewProfile />
+              </ProtectedRoute>
+            }
+          />
 
-    return (
-        <div className="app">
-            <Router>
-                <Routes>
-                    <Route path="/" element={<Login />} />
-                    <Route path="/forgot" element={<Forgot />} />
-                    <Route path="/search" element={
-                        <ProtectedRoute user={user}>
-                            <Search />
-                        </ProtectedRoute>
-                    } />
-                    <Route path="/ViewRecord/:searchQuery" element={
-                        <ProtectedRoute user={user}>
-                            <ViewRecord />
-                        </ProtectedRoute>
-                    } />
-                    <Route path="/ViewRecord" element={
-                        <ProtectedRoute user={user}>
-                            <ViewRecord />
-                        </ProtectedRoute>
-                    } />
+          <Route
+            path="/AddRecord"
+            element={
+              <ProtectedRoute user={user}>
+                <AddRecord />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/AddRecordTest"
+            element={
+              // to be removed
+              <ProtectedRoute user={user}>
+                <AddRecordTest />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/AddRecordFinal"
+            element={
+              // FINAL VERSION
+              <ProtectedRoute user={user}>
+                <AddRecordFinal />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/Payroll"
+            element={
+              <ProtectedRoute user={user}>
+                <Payroll />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/PayrollFinal"
+            element={
+              //to be final version
+              <ProtectedRoute user={user}>
+                <PayrollFinal />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/ArchiveRecord"
+            element={
+              <ProtectedRoute user={user}>
+                <ArchiveRecord />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/EmployeeProfile/:userEmployeeID"
+            element={
+              <ProtectedRoute user={user}>
+                <EmployeeProfile />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="*" element={<NotFound />} />
+          <Route path="/Unauthorized" element={<Unauthorized />} />
+          <Route path="/DTR" element={<DTR />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
 
-                    <Route path="/ViewProfile/:employeeID" element={
-                        <ProtectedRoute user={user}>
-                            <ViewProfile />
-                        </ProtectedRoute>
-                    }/>
-
-                    <Route path="/AddRecord" element={
-                        <ProtectedRoute user={user}>
-                            <AddRecord  />
-                        </ProtectedRoute>
-                    } />
-                    <Route path="/AddRecordTest" element={// to be removed
-                        <ProtectedRoute user={user}>
-                            <AddRecordTest />
-                        </ProtectedRoute>
-                    } />
-                    <Route path="/AddRecordFinal" element={// FINAL VERSION
-                        <ProtectedRoute user={user}>
-                            <AddRecordFinal />
-                        </ProtectedRoute>
-                    } />
-                    <Route path="/Payroll" element={
-                        <ProtectedRoute user={user}>
-                            <Payroll />
-                        </ProtectedRoute>
-                    } />
-                    <Route path="/PayrollFinal" element={ //to be final version
-                        <ProtectedRoute user={user}>
-                            <PayrollFinal />
-                        </ProtectedRoute>
-                    } />
-                    <Route path="/ArchiveRecord" element={
-                        <ProtectedRoute user={user}>
-                            <ArchiveRecord />
-                        </ProtectedRoute>
-                    } />
-                    <Route path="/EmployeeProfile" element={
-                        <ProtectedRoute user={user}>
-                            <EmployeeProfile />
-                        </ProtectedRoute>
-                    } />
-                    <Route path="/EmployeeProfile/:userEmployeeID" element={
-                        <ProtectedRoute user={user}>
-                            <EmployeeProfile />
-                        </ProtectedRoute>
-                    } />
-                    <Route path="*" element={<NotFound />}/>
-                    <Route path="/DTR" element={<DTR />}/>
-                </Routes>
-            </Router>
-        </div>
-    )
-}
- 
 export default App;
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,31 +1,31 @@
-import React, { useState, useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import './login.css'; 
-import { auth, db} from "../firebase";
-import { collection, getDocs, doc, setDoc, getDoc, deleteDoc } from 'firebase/firestore';
-import { signInWithEmailAndPassword, signOut } from 'firebase/auth';
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import "./login.css";
+import { auth, db } from "../firebase";
+import { collection, getDocs } from "firebase/firestore";
+import { signInWithEmailAndPassword, signOut } from "firebase/auth";
 
 const Login = () => {
-  const [username, setUsername] = useState(''); 
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const [passwordVisible, setPasswordVisibility] = useState(false);
   const [errorMsg, setErrorMsg] = useState(null);
   const [loginAttempts, setLoginAttempts] = useState(0);
-  const [waitTime, setWaitTime] = useState(0); 
+  const [waitTime, setWaitTime] = useState(0);
 
-  const navigate = useNavigate(); 
- 
+  const navigate = useNavigate();
+
   const passwordVisibility = () => {
     setPasswordVisibility(!passwordVisible);
   };
 
   useEffect(() => {
-    document.body.classList.add('loginPage');
+    document.body.classList.add("loginPage");
 
     return () => {
-      document.body.classList.remove('loginPage');
+      document.body.classList.remove("loginPage");
     };
-  }, []); 
+  }, []);
 
   useEffect(() => {
     // Automatically log out when the Login component mounts
@@ -41,48 +41,60 @@ const Login = () => {
   const handleLogin = async () => {
     if (!username || !password) return;
     signInWithEmailAndPassword(auth, username, password)
-      .then(async(userCredential) => {
+      .then(async (userCredential) => {
         const user = userCredential.user;
-        {/*console.log(user);
-        navigate('/search');*/}
+        {
+          /*console.log(user);
+        navigate('/search');*/
+        }
         // Retrieve role information from Firestore
         try {
-          const querySnapshot = await getDocs(collection(db, 'employees_active'));
-          const results = querySnapshot.docs.map(doc => {
-            const data = doc.data();
-            // Check if 'email' matches the provided username
-            if (data && data.email === username) {
-              return { id: doc.id, role: data.role, employeeID: data.employeeID };
-            } else {
+          const querySnapshot = await getDocs(
+            collection(db, "employees_active")
+          );
+          const results = querySnapshot.docs
+            .map((doc) => {
+              const data = doc.data();
+              // Check if 'email' matches the provided username
+              if (data && data.email === username) {
+                return {
+                  id: doc.id,
+                  role: data.role,
+                  employeeID: data.employeeID,
+                };
+              } else {
                 return null; // Or handle the case where 'role' field is missing
-            }
-          }).filter(Boolean); // Remove null entries
+              }
+            })
+            .filter(Boolean); // Remove null entries
 
           if (results.length > 0) {
             // If user data exists with matching email/username
             const userRole = results[0].role;
             const userEmployeeID = results[0].employeeID;
+            const loggedInEmployeeID = results[0].employeeID;
             // Perform actions based on user role
-            if (userRole === 'admin') {
-                navigate('/search');
-            } else if (userRole === 'employee') {
-                navigate(`/EmployeeProfile/${userEmployeeID}`);
+            if (userRole === "admin") {
+              navigate("/search");
+            } else if (userRole === "employee") {
+              navigate(`/EmployeeProfile/${userEmployeeID}`, {
+                state: { loggedInEmployeeID },
+              });
             } else {
-                console.log('User role not recognized');
+              console.log("User role not recognized");
             }
-        } else {
-          setErrorMsg("Invalid username or password. Please try again.");
-          setLoginAttempts(prevAttempts => prevAttempts + 1);
-          if (loginAttempts === 2 && waitTime === 0) {
-            setWaitTime(60); 
-            startCountdown(); 
+          } else {
+            setErrorMsg("Invalid username or password. Please try again.");
+            setLoginAttempts((prevAttempts) => prevAttempts + 1);
+            if (loginAttempts === 2 && waitTime === 0) {
+              setWaitTime(60);
+              startCountdown();
+            }
+            console.log(loginAttempts);
+            console.log("No user found with the provided email/username");
           }
-          console.log(loginAttempts)
-          console.log('No user found with the provided email/username');
-        }
-
         } catch (error) {
-          console.error('Error fetching records:', error);
+          console.error("Error fetching records:", error);
         }
       })
       .catch((error) => {
@@ -90,34 +102,34 @@ const Login = () => {
         const errorMessage = error.message;
         console.log(errorCode, errorMessage);
         setErrorMsg("Invalid username or password. Please try again.");
-        setLoginAttempts(prevAttempts => prevAttempts + 1);
+        setLoginAttempts((prevAttempts) => prevAttempts + 1);
         if (loginAttempts === 2 && waitTime === 0) {
-          setWaitTime(60); 
-          startCountdown(); 
+          setWaitTime(60);
+          startCountdown();
         }
-        console.log(loginAttempts)
+        console.log(loginAttempts);
       });
   };
 
   const startCountdown = () => {
     if (waitTime > 0) {
       setTimeout(() => {
-        setWaitTime(prevWaitTime => prevWaitTime - 1);
-      }, 1000); 
+        setWaitTime((prevWaitTime) => prevWaitTime - 1);
+      }, 1000);
     }
   };
 
   useEffect(() => {
     if (waitTime > 0) {
       const countdownInterval = setInterval(() => {
-        setWaitTime(prevWaitTime => prevWaitTime - 1);
+        setWaitTime((prevWaitTime) => prevWaitTime - 1);
       }, 1000);
-      return () => clearInterval(countdownInterval); 
+      return () => clearInterval(countdownInterval);
     }
   }, [waitTime]);
 
   useEffect(() => {
-    if (loginAttempts === 2 && waitTime === 0 ) {
+    if (loginAttempts === 2 && waitTime === 0) {
       startCountdown();
     }
   }, [loginAttempts, waitTime]);
@@ -129,9 +141,9 @@ const Login = () => {
       }, 1000);
 
       return () => clearInterval(interval);
-    }else if (loginAttempts >= 3 && loginAttempts <= 5 && waitTime === 0) {
+    } else if (loginAttempts >= 3 && loginAttempts <= 5 && waitTime === 0) {
       setErrorMsg("Invalid username or password. Please try again.");
-    }else if (loginAttempts >= 6){
+    } else if (loginAttempts >= 6) {
       setErrorMsg("Reset your password.");
     }
   }, [loginAttempts, waitTime]);
@@ -141,9 +153,9 @@ const Login = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-  }
+  };
 
-  return ( 
+  return (
     <>
       <div className="login-page">
         <div className="group-left">
@@ -165,44 +177,85 @@ const Login = () => {
             <input
               type="text"
               placeholder="Enter Username"
-              className={`username ${errorMsg ? 'error-input' : ''}`} 
+              className={`username ${errorMsg ? "error-input" : ""}`}
               name="username"
               autoComplete="username"
               required
-              value = {username} onChange={handleUsername}
+              value={username}
+              onChange={handleUsername}
             />
 
             <input
-              type={passwordVisible ? 'text' : 'password'}
+              type={passwordVisible ? "text" : "password"}
               placeholder="Enter Password"
-              className={`password ${errorMsg ? 'error-input' : ''}`}
+              className={`password ${errorMsg ? "error-input" : ""}`}
               id="input"
               name="password"
               autoComplete="current-password"
               required
-              value = {password} onChange={handlePassword}
+              value={password}
+              onChange={handlePassword}
             />
             {passwordVisible ? (
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="eye-btn" onClick={passwordVisibility}>
-                <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
-                <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"/>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="eye-btn"
+                onClick={passwordVisibility}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"
+                />
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                />
               </svg>
             ) : (
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="eye-slash-btn" onClick={passwordVisibility}>
-                <path strokeLinecap="round" strokeLinejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="eye-slash-btn"
+                onClick={passwordVisibility}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88"
+                />
               </svg>
             )}
 
-            <button className="login-button" onClick={handleLogin} disabled={waitTime > 0 || loginAttempts >= 6}>
+            <button
+              className="login-button"
+              onClick={handleLogin}
+              disabled={waitTime > 0 || loginAttempts >= 6}
+            >
               LOGIN
             </button>
 
             {errorMsg && (
-              <p className={
-                errorMsg.includes('Please try again in') ? 'countdown-error' :
-                errorMsg === "Invalid username or password. Please try again." ? 'invalid-credentials' :
-                errorMsg === "Reset your password." ? 'reset' : 'error-message'
-              }>
+              <p
+                className={
+                  errorMsg.includes("Please try again in")
+                    ? "countdown-error"
+                    : errorMsg ===
+                      "Invalid username or password. Please try again."
+                    ? "invalid-credentials"
+                    : errorMsg === "Reset your password."
+                    ? "reset"
+                    : "error-message"
+                }
+              >
                 {errorMsg}
               </p>
             )}
